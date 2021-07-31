41976aa8e265868865e0bc45c1552c528f99734f
NuclearNadal <1magination@gmail.com>
2021-07-31 22:28:42 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# 4k-fluid-responsive README
+
+Converts pixels to vws, removes properties that have no value in pixes, and removes classes that have no properties left, from a selection of markup and places formated markup into clipboard. Markup can be pasted into a media query for screens bigger than 1920px.
+
+## Usage
+
+![Usage](usage.gif)
+
+4K Fluid Responsive has one command, `Convert Pixels to VWs`. Select some HTML markup, run the command and the transformed SCSS markup is added to your clipboard in the order they appear. Output is formated by Prettier. The default devider (the base width of the design) for calculating values is 1920, that is configurable in settings.
+
+## Requirements
+
+Uses the [ncp](https://github.com/xavi-/node-copy-paste) package, may require external dependencies in Linux and macOS.
+
+## Future Plans
+
+- [ ] Make it work with CSS and SASS files also
---
+++ extension.js
@@ -0,0 +1,135 @@
+const vscode = require('vscode');
+const cpt = require('copy-paste');
+const { parse, stringify } = require('scss-parser')
+const prettier = require('prettier');
+
+let configuration = vscode.workspace.getConfiguration("4k-fluid-responsive", vscode.Uri);
+let baseDesignWidth = configuration.get("designWidth");
+
+const formatRule = (rule) => {
+	const filteredRule = rule.value[1].value.filter(val => {
+		if(val.type === 'rule') {
+			return formatRule(val);
+		} else if (val.type === 'declaration') {
+			return formatDelaration(val);
+		} else {
+			return false;
+		}
+	})
+	if(filteredRule.length > 0) {
+		rule.value[1].value = filteredRule;
+		return true;
+	} else {
+		return false;
+	}
+}
+
+const formatDelaration = (declaration) => {
+	let value;
+	declaration.value.forEach(val => {
+		if(val.type === 'value') {
+			value = val;
+		}
+	})
+	if(!value) return false;
+
+	let indetifier;
+	let func;
+	value.value.forEach(val => {
+		if(val.type === 'identifier' && val.value === 'px') {
+			indetifier = true;
+		}
+		if(val.type === 'function') {
+			func = true;
+		}
+	})
+	
+	if(!indetifier && !func) return false;
+	
+	if(indetifier) {
+		let newValue = [];
+		value.value.forEach((val, index) => {
+			let newVal = {...val};
+			if(val.type === 'number' && value.value[index + 1].value === 'px') {
+				const vwValue = (parseInt(val.value) / baseDesignWidth * 100).toFixed(5).toString()
+				newVal.value = vwValue;
+			}
+			if(val.type === 'identifier' && val.value === 'px') {
+				newVal.value = 'vw';
+			}
+			newValue.push(newVal);
+		})
+		value.value = newValue;
+		return true;
+	}
+	if(func) {
+		let newValue = [];
+		let hasPxInside = false;
+		value.value.forEach(val => {
+			let newVal = {...val};
+			let newArgs = [];
+			if(val.type === "function") {
+				val.value[1].value.forEach((arg, index) => {
+					const newArg = {...arg};
+					if(val.value[1].value[index + 1]) {
+						if(arg.type === 'number' && val.value[1].value[index + 1].value === 'px') {
+							const vwValue = (parseInt(arg.value) / baseDesignWidth * 100).toFixed(5).toString()
+							newArg.value = vwValue;
+							hasPxInside = true;
+						} 
+					}
+					if(arg.type === 'identifier' && arg.value === 'px') {
+						newArg.value = 'vw';
+					}
+					newArgs.push(newArg);
+				})
+				val.value[1].value = newArgs;
+				newValue.push(newVal);
+			}
+		})
+		value.value = newValue;
+		if(!hasPxInside) return false;
+		return true;
+	}
+}
+
+function activate(context) {
+
+	let disposable = vscode.commands.registerCommand('4k-fluid-responsive.ConvertToVWs', function () {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) return; 
+
+		configuration = vscode.workspace.getConfiguration("4k-fluid-responsive", vscode.Uri);
+		baseDesignWidth = configuration.get("designWidth");
+
+		const selectedText = editor.document.getText(editor.selection);
+		const parsedText = parse(selectedText);
+		const formatedText = parsedText.value.filter(val => {
+			if(val.type === 'rule') {
+				return formatRule(val);
+			} else if (val.type === 'declaration') {
+				return formatDelaration(val);
+			} else {
+				return false;
+			}
+		})
+		parsedText.value = formatedText;
+		const finalText = stringify(parsedText);
+		const prettierText = prettier.format(finalText, {
+			parser: "scss"
+		});
+
+		cpt.copy(prettierText, ()=> {
+			vscode.window.showInformationMessage('Pixels have been converted to VWs!');
+		})
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
